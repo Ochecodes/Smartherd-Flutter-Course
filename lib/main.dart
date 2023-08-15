93da22d72000c02bf15ded2d8da9e180219d302e
Ochecodes <ochebooks@gmail.com>
2023-08-15 05:40:00 -0700

    Added MaterialApp, and Text Widget Properties

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,24 @@ import "package:flutter/material.dart";
 void main() {
   
   runApp(
-    const Center(
-      child: Text(
-         "Hello Flutter", 
-           textDirection: TextDirection.ltr
-      )
-    ) //text
+
+    const MaterialApp(
+      title: "My Flutter App", 
+      home: Material(
+        color: Colors.grey,
+        child: Center(
+             child: Text(
+                  "Hello Flutter", 
+                  textDirection: TextDirection.ltr, 
+                  style: TextStyle(
+                    color: Colors.black, 
+                    fontSize: 50.8, 
+                    fontWeight: 
+                    FontWeight.bold,
+                  ),
+              ),
+        ),
+      ) //text
+    )
   );
 }
